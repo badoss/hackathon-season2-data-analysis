b576f5b48815feee101801e2b3708bcaa64fde27
boo boo <thongrit.th@inet.co.th>
2022-10-08 20:10:48 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hackathon-seson2-data-analysis
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
