8da246c86ae1501d2d65df27f1df77ce1caa5ebd
boo boo <thongrit.th@inet.co.th>
2022-10-08 20:35:52 +0700

    update lib

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
         Hackathon-seson2-data-analysis
         </p>
 
       </header>
+      <footer className="App-footer">
+        BSMx team
+      </footer>
     </div>
   );
 }
